770998532cd42723c166ea698ec188955ed49208
Unknown <dominique.sydow@posteo.de>
2019-08-02 14:23:32 +0200

    Update preprocessing
    * Drop entries not in DFG-in
    * Drop entries without mol2 or pdb
    * Update gap rate plot style
    * Modulize steps in separate functions

--- kinsim_structure/preprocessing.py
+++ kinsim_structure/preprocessing.py
@@ -15,7 +15,7 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 
 
-def get_klifs_data_from_files(klifs_overview_file, klifs_export_file, remove_subpocket_columns=True):
+def get_klifs_metadata_from_files(klifs_overview_file, klifs_export_file, remove_subpocket_columns=True):
     """
     Get KLIFS metadata as DataFrame.
     
@@ -111,7 +111,7 @@ def get_klifs_data_from_files(klifs_overview_file, klifs_export_file, remove_sub
     
     # Merge on mutual columns
     
-    klifs_data = klifs_export.merge(
+    klifs_metadata = klifs_export.merge(
         right=klifs_overview,
         how='outer',
         on=['species',
@@ -123,79 +123,218 @@ def get_klifs_data_from_files(klifs_overview_file, klifs_export_file, remove_sub
             'ligand_allosteric_pdb_id']
     )
     
-    if not klifs_overview.shape[0] == klifs_export.shape[0] == klifs_data.shape[0]:
+    if not klifs_overview.shape[0] == klifs_export.shape[0] == klifs_metadata.shape[0]:
         raise ValueError(f'Output table has incorrect number of rows:'
                          f'KLIFS overview table has shape: {klifs_overview.shape}'
                          f'KLIFS export table has shape: {klifs_export.shape}'
-                         f'KLIFS merged table has shape: {klifs_data.shape}')
+                         f'KLIFS merged table has shape: {klifs_metadata.shape}')
     
-    if not (klifs_overview.shape[1] + klifs_export.shape[1] - 7) == klifs_data.shape[1]:
+    if not (klifs_overview.shape[1] + klifs_export.shape[1] - 7) == klifs_metadata.shape[1]:
         raise ValueError(f'Output table has incorrect number of columns'
                          f'KLIFS overview table has shape: {klifs_overview.shape}'
                          f'KLIFS export table has shape: {klifs_export.shape}'
-                         f'KLIFS merged table has shape: {klifs_data.shape}')
+                         f'KLIFS merged table has shape: {klifs_metadata.shape}')
         
     # Remove subpocket columns
     if remove_subpocket_columns:
-        klifs_data.drop(labels=klifs_data.columns[21:], axis=1, inplace=True)
+        klifs_metadata.drop(labels=klifs_metadata.columns[21:], axis=1, inplace=True)
         
-    return klifs_data
+    return klifs_metadata
 
 
-def filter_klifs_data(klifs_data, species='Human', drop_duplicate_pdb_ids_per_kinase=True):
+def get_species(klifs_metadata, species='Human'):
     """
-    Filter KLIFS dataset. Currently filter steps include:
-    - Filter by species
-    - Keep only the KLIFS entry per kinase-PDB ID combination with the best quality score
-    
+    Filter KLIFS dataset by species.
+
     Parameters
     ----------
-    klifs_data : pandas.DataFrame
+    klifs_metadata : pandas.DataFrame
         DataFrame containing merged metadate from both input KLIFS tables.
     species : str
         String for species name.
-    drop_duplicate_pdb_ids_per_kinase : bool
-        Set to True (default) to keep only first PDB ID for each kinase.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing merged metadata from both input KLIFS tables filtered by certain criteria.
+    """
+
+    klifs_metadata_filtered = klifs_metadata.copy()
+
+    # Select species
+    if species in klifs_metadata_filtered.species.unique():
+        klifs_metadata_filtered.drop(
+            klifs_metadata_filtered[klifs_metadata_filtered.species != species].index,
+            inplace=True
+        )
+    else:
+        raise ValueError(f'Species {species} not in species list: '
+                         f'{", ".join(klifs_metadata_filtered.species.unique())}')
+
+    return klifs_metadata_filtered
+
+
+def get_dfg(klifs_metadata, dfg='in'):
+    """
+    Filter KLIFS dataset by DFG loop position.
+
+    Parameters
+    ----------
+    klifs_metadata : pandas.DataFrame
+        DataFrame containing merged metadate from both input KLIFS tables.
+    dfg : str
+        String for DFG loop position.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing merged metadata from both input KLIFS tables filtered by certain criteria.
+    """
+
+    klifs_metadata_filtered = klifs_metadata.copy()
+
+    # Select DFG conformation
+    if dfg in klifs_metadata_filtered.dfg.unique():
+        klifs_metadata_filtered.drop(
+            klifs_metadata_filtered[klifs_metadata_filtered.dfg != dfg].index,
+            inplace=True
+        )
+    else:
+        raise ValueError(f'DFG position {dfg} not in DFG list: {", ".join(klifs_metadata_filtered.dfg.unique())}')
+
+    return klifs_metadata_filtered
+
+
+def get_unique_pdbid_per_kinase(klifs_metadata):
+    """
+    Filter KLIFS dataset by keeping only the KLIFS entry per kinase-PDB ID combination with the best quality score.
+    
+    Parameters
+    ----------
+    klifs_metadata : pandas.DataFrame
+        DataFrame containing merged metadate from both input KLIFS tables.
         
     Returns
     -------
     pandas.DataFrame
-        DataFrame containing merged metadate from both input KLIFS tables filtered by certain criteria.
+        DataFrame containing merged metadata from both input KLIFS tables filtered by certain criteria.
     """
     
-    klifs_data_filtered = klifs_data.copy()
-    
-    # Select species
-    if species in list(klifs_data_filtered.species):
-        klifs_data_filtered.drop(klifs_data_filtered[klifs_data_filtered.species != species].index, inplace=True)
-
-    if drop_duplicate_pdb_ids_per_kinase:
+    klifs_metadata_filtered = klifs_metadata.copy()
         
-        # For each kinase and PDB IDs with multiple KLIFS entries (structures), 
-        # select entry with the best quality score 
-        
-        # Sort by kinase, PDB ID and quality score 
-        # (so that for multiple equal kinase-pdb_id combos, highest quality score will come first)
-        klifs_data_filtered.sort_values(by=['kinase', 'pdb_id', 'qualityscore'], 
-                                        ascending=[True, True, False], 
-                                        inplace=True)
-        # Drop duplicate kinase-pdb_id combos and keep only first (with highest quality score)
-        klifs_data_filtered.drop_duplicates(subset=['kinase', 'pdb_id'], 
-                                            keep='first',
-                                            inplace=True)
-        # Reset DataFrame indices
-        klifs_data_filtered.reset_index(inplace=True)
+    # For each kinase and PDB IDs with multiple KLIFS entries (structures),
+    # select entry with the best quality score
 
-    return klifs_data_filtered
+    # Sort by kinase, PDB ID and quality score
+    # (so that for multiple equal kinase-pdb_id combos, highest quality score will come first)
+    klifs_metadata_filtered.sort_values(
+        by=['kinase', 'pdb_id', 'qualityscore'],
+        ascending=[True, True, False],
+        inplace=True
+    )
+    # Drop duplicate kinase-pdb_id combos and keep only first (with highest quality score)
+    klifs_metadata_filtered.drop_duplicates(
+        subset=['kinase', 'pdb_id'],
+        keep='first',
+        inplace=True
+    )
+    # Reset DataFrame indices
+    klifs_metadata_filtered.reset_index(inplace=True)
+
+    return klifs_metadata_filtered
 
 
-def calculate_gap_rate(klifs_data):
+def drop_missing_mol2s(klifs_metadata, path_to_data):
+    """
+    Drop entries in KLIFS metadata that have no corresponding mol2 file.
+
+    Parameters
+    ----------
+    klifs_metadata : pandas.DataFrame
+        DataFrame containing merged metadate from both input KLIFS tables.
+    path_to_data : str or pathlib.Path
+        Path to directory of KLIFS dataset files.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing merged metadata from both input KLIFS tables filtered by certain criteria.
+    """
+
+    path_to_data = Path(path_to_data) / 'raw' / 'KLIFS_download'
+
+    klifs_metadata_filtered = klifs_metadata.copy()
+
+    indices = []
+
+    for index, row in klifs_metadata_filtered.iterrows():
+
+        # Depending on whether alternate model and chain ID is given build file path:
+        mol2_path = path_to_data / row.species.upper() / row.kinase
+
+        if row.alternate_model != '-' and row.chain != '-':
+            mol2_path = mol2_path / f'{row.pdb_id}_alt{row.alternate_model}_chain{row.chain}' / 'pocket.mol2'
+        elif row.alternate_model == '-' and row.chain != '-':
+            mol2_path = mol2_path / f'{row.pdb_id}_chain{row.chain}' / 'pocket.mol2'
+        elif row.alternate_model == '-' and row.chain == '-':
+            mol2_path = mol2_path / f'{row.pdb_id}' / 'pocket.mol2'
+        else:
+            raise ValueError(f'{row.alternate_model}, {row.chain}')
+
+        # Not all paths exist - save list with missing paths
+        if not mol2_path.exists():
+            indices.append(index)
+
+    klifs_metadata_filtered.drop(indices, inplace=True)
+
+    return klifs_metadata_filtered
+
+
+def drop_missing_pdbs(klifs_metadata, path_to_data):
+    """
+    Drop entries in KLIFS metadata that have no corresponding pdb file.
+
+    Parameters
+    ----------
+    klifs_metadata : pandas.DataFrame
+        DataFrame containing merged metadate from both input KLIFS tables.
+
+    path_to_data : str or pathlib.Path
+        Path to directory of KLIFS dataset files.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing merged metadata from both input KLIFS tables filtered by certain criteria.
+    """
+
+    path_to_data = Path(path_to_data) / 'raw' / 'PDB_download'
+
+    klifs_metadata_filtered = klifs_metadata.copy()
+
+    indices = []
+
+    for index, row in klifs_metadata_filtered.iterrows():
+
+        # Depending on whether alternate model and chain ID is given build file path:
+        cif_path = path_to_data / f'{row.pdb_id}.cif'
+
+        # Not all paths exist - save list with missing paths
+        if not cif_path.exists():
+            indices.append(index)
+
+    klifs_metadata_filtered.drop(indices, inplace=True)
+
+    return klifs_metadata_filtered
+
+
+def calculate_gap_rate(klifs_metadata):
     """
     Calculate gap rate at every KLIFS MSA position across the filtered kinase data set
     
     Parameters
     ----------
-    klifs_data : pandas.DataFrame
+    klifs_metadata : pandas.DataFrame
         DataFrame containing merged metadate from both input KLIFS tables.
         
     Returns
@@ -206,12 +345,12 @@ def calculate_gap_rate(klifs_data):
 
     gaps = [0] * 85
     
-    for pocket in klifs_data.pocket:
+    for pocket in klifs_metadata.pocket:
         for klifs_position, residue in enumerate(pocket):
             if residue == '_':
                 gaps[klifs_position] += 1
 
-    gap_rate = [round(i/float(klifs_data.shape[0]), 4) for i in gaps]
+    gap_rate = [round(i/float(klifs_metadata.shape[0]), 4) for i in gaps]
     
     return pd.concat([pd.Series(range(1, 86), name='klifs_position'),
                       pd.Series(gaps, name='gaps'),  
@@ -219,13 +358,13 @@ def calculate_gap_rate(klifs_data):
                      axis=1)
 
 
-def download_from_pdb(klifs_data, output_path):
+def download_from_pdb(klifs_metadata, output_path):
     """
     Download structure files from the PDB for KLIFS dataset.
     
     Parameters
     ----------
-    klifs_data : pandas.DataFrame
+    klifs_metadata : pandas.DataFrame
         DataFrame containing merged metadate from both input KLIFS tables.
     output_path : str or pathlib.Path
         Path to output directory.
@@ -233,7 +372,9 @@ def download_from_pdb(klifs_data, output_path):
     
     pdbfile = PDBList()
     
-    for index, row in klifs_data.iterrows():
+    for index, row in klifs_metadata.iterrows():
         if not (Path(output_path) / f'{row.pdb_id}.cif').exists():
             print(Path(output_path) / f'{row.pdb_id}.cif')
             pdbfile.retrieve_pdb_file(row.pdb_id, pdir=output_path)
+        else:
+            continue
