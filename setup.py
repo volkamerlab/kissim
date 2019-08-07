a0669c2139452d7f2859702148beee283e5fa21a
Unknown <dominique.sydow@posteo.de>
2019-08-07 17:22:24 +0200

    Update setup.py (as in ratar)

--- setup.py
+++ setup.py
@@ -34,12 +34,13 @@ setup(
     # Which Python importable modules should be included when your package is installed
     # Handled automatically by setuptools. Use 'exclude' to prevent some specific
     # subpackage(s) from being added, if needed
-    packages=find_packages(),
+    packages=['kinsim_structure', 'kinsim_structure.tests'],
 
     # Optional include package data to ship with your package
     # Customize MANIFEST.in if the general case does not suit your needs
     # Comment out this line to prevent the files from being packaged with your software
-    include_package_data=True,
+    package_data={'kinsim_structure': ["data/*.dat"]
+                  },
 
     # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
