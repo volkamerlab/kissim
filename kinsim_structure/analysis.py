b98bf8537988de397914553989bf7e80d440993a
Unknown <dominique.sydow@posteo.de>
2019-08-07 20:41:45 +0200

    Update function according to new encoding architecture

--- kinsim_structure/analysis.py
+++ kinsim_structure/analysis.py
@@ -6,13 +6,13 @@ Subpocket-based structural fingerprint for kinase pocket comparison.
 Handles the primary functions for the structural kinase fingerprint analysis.
 """
 
-from collections import defaultdict
 import logging
+from pathlib import Path
 
 import pandas as pd
 
-from kinsim_structure.auxiliary import MoleculeLoader, get_mol2paths_from_metadata
-from kinsim_structure.encoding import get_ca_cb_com_vectors, get_side_chain_orientation
+from kinsim_structure.auxiliary import KlifsMoleculeLoader
+from kinsim_structure.encoding import SideChainOrientationFeature
 
 logger = logging.getLogger(__name__)
 
@@ -61,9 +61,13 @@ def get_non_standard_amino_acids_in_klifs(klifs_metadata):
         List of non-standard amino acids (value) for each structure contained in the input mol2 file (key).
     """
 
-    # Input parameters
-    mol2_paths = get_mol2paths_from_metadata(klifs_metadata)
-    missing_paths = []
+    # Get list of molecules linked to metadata
+
+    molecules = []
+    for index, row in klifs_metadata.iterrows():
+        klifs_molecule_loader = KlifsMoleculeLoader(metadata_entry=row)
+        molecule = klifs_molecule_loader.molecule
+        molecules.append(molecule)
 
     # 20 standard amino acids
     standard_aminoacids = 'ALA ARG ASN ASP CYS GLN GLU GLY HIS ILE LEU LYS MET PHE PRO SER THR TRP TYR VAL'.split()
@@ -71,15 +75,7 @@ def get_non_standard_amino_acids_in_klifs(klifs_metadata):
     # Will contain per structure (key) list of non-standard amino acids (values)
     non_standard_aminoacids = {}
 
-    for mol2_path in mol2_paths:
-
-        if not mol2_path.exists():
-            print(f'Path does not exist: {mol2_path}')
-            missing_paths.append(mol2_path)
-            continue
-
-        molecule_loader = MoleculeLoader(mol2_path)
-        molecule = molecule_loader.molecules[0]
+    for molecule in molecules:
 
         # Get deduplicated amino acid names
         res_names = set(molecule.df.res_name)
@@ -94,37 +90,8 @@ def get_non_standard_amino_acids_in_klifs(klifs_metadata):
     return non_standard_aminoacids
 
 
-def get_missing_ca_cb_stats(klifs_metadata):
-    """
-    """
-
-    mol2_paths = get_mol2paths_from_metadata(klifs_metadata)
-    missing_paths = []
-
-    stats = []
-
-    for mol2_path in mol2_paths:
-
-        if not mol2_path.exists():
-            print(f'Path does not exist: {mol2_path}')
-            missing_paths.append(mol2_path)
-            continue
-
-        # Load data from file
-        molecule_loader = MoleculeLoader(mol2_path)
-        molecule = molecule_loader.molecules[0]
-
-        points = get_ca_cb_com_vectors(molecule)
-        points['klifs_code'] = molecule.code
-
-        stats.append(points)
-
-    return pd.concat(stats)
-
-
 def get_side_chain_orientation_stats(klifs_metadata):
     """
-    Collect all side chain orientations in KLIFS dataset, ordered by residue name (e.g. ALA).
 
     Parameters
     ----------
@@ -133,39 +100,24 @@ def get_side_chain_orientation_stats(klifs_metadata):
 
     Returns
     -------
-    dict
-        For each residue name, list observed angles plus metadata, i.e. molecule and residue code.
+    pandas.DataFrame
+        CA, CB and centroid points for each residue for all molecules described in the KLIFS metadata.
     """
 
-    mol2_paths = get_mol2paths_from_metadata(klifs_metadata)
-    missing_paths = []
+    stats = []
 
-    stats = defaultdict(pd.DataFrame)
+    for index, row in klifs_metadata.iterrows():
 
-    for mol2_path in mol2_paths:
+        print(f'{index+1}/{len(klifs_metadata)}')
 
-        if not mol2_path.exists():
-            print(f'Path does not exist: {mol2_path}')
-            missing_paths.append(mol2_path)
-            continue
+        klifs_molecule_loader = KlifsMoleculeLoader(metadata_entry=row)
+        molecule = klifs_molecule_loader.molecule
 
-        # Load data from file
-        molecule_loader = MoleculeLoader(mol2_path)
-        molecule = molecule_loader.molecules[0]
+        side_chain_orientation_feature = SideChainOrientationFeature(molecule)
+        points = side_chain_orientation_feature.from_molecule(molecule, verbose=True)
 
-        # Get residue code (ALA50), residue name (ALA) and side chain orientations (angles)
-        subst_names = molecule.df.groupby(by='subst_name')['res_id'].first().index  # ALA50
-        residue_names = molecule.df.groupby(by='subst_name')['res_name'].first().values  # ALA
-        angles = get_side_chain_orientation(molecule)  # Angles in degrees
+        points['klifs_code'] = molecule.code
 
-        # For each residue save residue code, residue name and angle as list
-        for subst_name, residue, angle in zip(subst_names, residue_names, angles):
-            stats[residue].append([molecule.code, subst_name, angle])
+        stats.append(points)
 
-    # Transform dict values from list of lists to pandas.DataFrame
-    for key, value in stats.items():
-        stats[key] = pd.DataFrame(value, columns=['molecule', 'residue', 'angle'])
-
-    print(f'Missing paths: {", ".join(missing_paths)}')
-
-    return stats
+    return pd.concat(stats)
