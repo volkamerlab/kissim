3a5ed45e3be725d90f75b731a74c958c03283484
Unknown <dominique.sydow@posteo.de>
2019-08-09 17:52:50 +0200

    Move all functions from notebook here in classes

--- kinsim_structure/analysis.py
+++ kinsim_structure/analysis.py
@@ -20,6 +20,43 @@ from kinsim_structure.encoding import SideChainOrientationFeature
 logger = logging.getLogger(__name__)
 
 
+class ResidueConservation:
+    """
+    Occurrence of each residue per KLIFS ID over all KLIFS structures.
+
+    Attributes
+    ----------
+    data : pandas.DataFrame
+        Occurrence of each residue (rows) over all structures per KLIFS ID (columns).
+
+    Parameters
+    ----------
+    klifs_metadata : pandas.DataFrame
+        DataFrame containing merged metadate from both input KLIFS tables.
+    """
+
+    def __init__(self, klifs_metadata):
+        self.data = self.get_residue_occurrence(klifs_metadata)
+
+    @staticmethod
+    def get_residue_occurrence(klifs_metadata):
+
+        # Get DataFrame containing residue per KLIFS ID (columns) for all structure pockets (rows).
+        pockets = pd.DataFrame(
+            list(klifs_metadata.pocket.apply(lambda x: list(x))),
+            columns=range(1, 86)
+        )
+
+        # Get DataFrame containing per KLIFS ID (columns) the occurrence of each residue (rows) over all structures.
+        residues_occurrence = pockets.apply(lambda x: x.value_counts(), axis=0)
+
+        # Convert float and nan values into int and 0 values
+        residues_occurrence.fillna(0, inplace=True)
+        residues_occurrence = residues_occurrence.astype('int64')
+
+        return residues_occurrence
+
+
 class GapRate:
     """
     ...
@@ -37,11 +74,10 @@ class GapRate:
 
     def __init__(self, klifs_metadata):
 
-        self.data = self.calculate_gap_rate(klifs_metadata)
         self.n_structures = klifs_metadata.shape[0]
+        self.data = self.calculate_gap_rate(klifs_metadata)
 
-    @staticmethod
-    def calculate_gap_rate(klifs_metadata):
+    def calculate_gap_rate(self, klifs_metadata):
         """
         Calculate gap rate at every KLIFS MSA position across the filtered kinase data set.
 
@@ -62,7 +98,8 @@ class GapRate:
             for klifs_position, residue in enumerate(pocket):
                 if residue == '_':
                     gaps[klifs_position] += 1
-        gap_rate = [round(i / float(klifs_metadata.shape[0]), 4) for i in gaps]
+
+        gap_rate = [round(i / float(self.n_structures), 4) for i in gaps]
 
         data = pd.concat(
             [
@@ -94,92 +131,222 @@ class GapRate:
                          y='gap_rate',
                          data=self.data,
                          color='steelblue')
+
         ax.set_title(f'KLIFS sequence alignment: '
                      f'Gap rate for the 85 residue positions ({self.n_structures} KLIFS entries)',
                      fontsize=20)
-        ax.set_xlabel('Alignment residue position')
+        ax.set_xlabel('KLIFS position ID')
         ax.set_ylabel('Gap rate')
-        ax.xaxis.set_ticks(np.arange(0, 85, 5));
-        ax.set_xticklabels(np.arange(0, 85, 5));
+        ax.xaxis.set_ticks(np.arange(4, 85, 5))
+        ax.set_xticklabels(np.arange(5, 86, 5))
 
         for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] + ax.get_xticklabels() + ax.get_yticklabels()):
             item.set_fontsize(20)
 
-        plt.savefig(path_to_results / 'plot_gap_rate.png', dpi=300)
+        plt.savefig(path_to_results / 'gap_rate.png', dpi=300)
 
 
-def get_non_standard_amino_acids_in_klifs(klifs_metadata):
-    """
-    For a given set of mol2 files, collect all non-standard amino acids.
+class SideChainOrientationStatistics:
 
-    Parameters
-    ----------
-    klifs_metadata : pandas.DataFrame
-        KLIFS metadata describing every pocket entry in the KLIFS dataset.
+    def __init__(self, klifs_metadata):
+        self.n_structures = klifs_metadata.shape[0]
+        self.data = None
+        self.missing_residues_ca_cb = None
 
-    Returns
-    -------
-    dict
-        List of non-standard amino acids (value) for each structure contained in the input mol2 file (key).
-    """
+    def from_file(self, path_to_results):
+        sco_stats = pd.read_pickle(path_to_results / 'stats_missing_ca_cb_and_sco.p')
+        self.data = sco_stats
 
-    # Get list of molecules linked to metadata
+    def to_file(self, path_to_results):
+        sco_stats = self.data
+        sco_stats.to_pickle(path_to_results / 'stats_missing_ca_cb_and_sco.p')
 
-    molecules = []
-    for index, row in klifs_metadata.iterrows():
-        klifs_molecule_loader = KlifsMoleculeLoader(metadata_entry=row)
-        molecule = klifs_molecule_loader.molecule
-        molecules.append(molecule)
+    def from_metadata(self, klifs_metadata):
+        """
 
-    # 20 standard amino acids
-    standard_aminoacids = 'ALA ARG ASN ASP CYS GLN GLU GLY HIS ILE LEU LYS MET PHE PRO SER THR TRP TYR VAL'.split()
+        Parameters
+        ----------
+        klifs_metadata : pandas.DataFrame
+            KLIFS metadata describing every pocket entry in the KLIFS dataset.
 
-    # Will contain per structure (key) list of non-standard amino acids (values)
-    non_standard_aminoacids = {}
+        Returns
+        -------
+        pandas.DataFrame
+            CA, CB and centroid points for each residue for all molecules described in the KLIFS metadata.
+        """
 
-    for molecule in molecules:
+        stats = []
 
-        # Get deduplicated amino acid names
-        res_names = set(molecule.df.res_name)
+        for index, row in klifs_metadata.iterrows():
+            print(f'{index + 1}/{len(klifs_metadata)}')
 
-        # Retain only non-standard amino acids
-        non_standard_aminoacids_per_file = [res_name for res_name in res_names if res_name not in standard_aminoacids]
+            klifs_molecule_loader = KlifsMoleculeLoader(metadata_entry=row)
+            molecule = klifs_molecule_loader.molecule
 
-        # If structure contains non-standard amino acids, add to dict
-        if non_standard_aminoacids_per_file:
-            non_standard_aminoacids[molecule.code] = non_standard_aminoacids_per_file
+            side_chain_orientation_feature = SideChainOrientationFeature(molecule)
+            points = side_chain_orientation_feature.from_molecule(molecule, verbose=True)
 
-    return non_standard_aminoacids
+            points['klifs_code'] = molecule.code
+
+            stats.append(points)
+
+        self.data = pd.concat(stats)
+
+    def get_missing_residues_ca_cb(self, gap_rate):
+
+        # Get number of missing atoms per KLIFS position
+        missing_ca = self.data[self.data.ca.isna()].groupby(by='klifs_id').size()
+        missing_cb = self.data[self.data.cb.isna()].groupby(by='klifs_id').size()
+        missing_ca_cb = self.data[(self.data.ca.isna()) & (self.data.cb.isna())].groupby(by='klifs_id', sort=False).size()
+
+        missing_positions = pd.DataFrame(
+            [
+                missing_ca,
+                missing_cb,
+                missing_ca_cb,
+                gap_rate.data.gaps
+            ],
+            index='ca cb ca_cb gaps'.split()
+        ).transpose()
+        missing_positions.fillna(value=0, inplace=True)
+        missing_positions = missing_positions.astype('int64')
+        missing_positions['klifs_id'] = missing_positions.index
+
+        self.missing_residues_ca_cb = missing_positions
+
+    def plot_missing_residues_ca_cb(self, path_to_results):
+
+        ax = plt.gca()
+
+        self.missing_residues_ca_cb.loc[:, ['cb', 'gaps']].plot(
+            figsize=(20, 6),
+            kind='bar',
+            stacked=True,
+            rot=1,
+            ax=ax
+        )
+
+        ax.set_title(f'Missing residues and Cb atoms ({self.n_structures} KLIFS entries)',
+                     fontsize=20)
+
+        ax.set_xlabel('KLIFS position ID')
+        ax.set_ylabel('Number of missing data')
+        ax.xaxis.set_ticks(np.arange(4, 85, 5))
+        ax.set_xticklabels(np.arange(5, 86, 5))
+
+        for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] + ax.get_xticklabels() + ax.get_yticklabels()):
+            item.set_fontsize(20)
+
+        plt.savefig(path_to_results / 'missing_residues_ca_cb.png', dpi=300)
+
+    def plot_side_chain_orientation_distribution(self, path_to_results, kind='violin'):
+
+        kinds = 'violin histograms'.split()
+
+        # 20 standard amino acids
+        standard_aminoacids = 'GLY ALA SER PRO VAL THR CYS ILE LEU ASN ASP GLN LYS GLU MET HIS PHE ARG TYR TRP'.split()
+
+        if kind == kinds[0]:
+
+            # Plot standard amino acids
+            plt.figure(figsize=(25, 8))
+
+            ax = sns.violinplot(
+                x='residue_name',
+                y='sco',
+                data=self.data[self.data.residue_name.isin(standard_aminoacids)],
+                order=standard_aminoacids
+            )
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=30)
+            ax.set_xlabel('Residue name (sorted by molecular weight)')
+            ax.set_ylabel('Side chain orientation angle')
+
+            for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] + ax.get_xticklabels() + ax.get_yticklabels()):
+                item.set_fontsize(20)
+
+            plt.savefig(path_to_results / 'sco_violin_standard.png', dpi=300)
+
+            # Plot non-standard amino acids
+            plt.figure(figsize=(20, 8))
+
+            ax = sns.violinplot(
+                x='residue_name',
+                y='sco',
+                data=self.data[~self.data.residue_name.isin(standard_aminoacids)]
+            )
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=30)
+            ax.set_xlabel('Residue name')
+            ax.set_ylabel('Side chain orientation angle')
+
+            for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] + ax.get_xticklabels() + ax.get_yticklabels()):
+                item.set_fontsize(20)
+
+            plt.savefig(path_to_results / 'sco_violin_nonstandard.png', dpi=300)
+
+        elif kind == kinds[1]:
+
+            plt.figure(figsize=(20, 30))
+
+            for index, residue in enumerate(standard_aminoacids, 1):
+                group = self.data.groupby(by='residue_name').get_group(residue)
+                plt.subplot(7, 3, index)
+                group.sco.plot(kind='hist', title=residue, xlim=(0, 180))
+
+            plt.savefig(path_to_results / 'sco_histograms.png', dpi=300, bbox_inches='tight')
+
+        else:
+
+            raise ValueError(f'Plot kind unknown. Please choose from: {", ".join(kinds)}')
 
 
-def get_side_chain_orientation_stats(klifs_metadata):
-    """
+class NonStandardKlifsAminoAcids:
 
-    Parameters
-    ----------
-    klifs_metadata : pandas.DataFrame
-        KLIFS metadata describing every pocket entry in the KLIFS dataset.
+    def __init__(self, klifs_metadata):
+        self.data = self.get_non_standard_amino_acids_in_klifs(klifs_metadata)
 
-    Returns
-    -------
-    pandas.DataFrame
-        CA, CB and centroid points for each residue for all molecules described in the KLIFS metadata.
-    """
+    @staticmethod
+    def get_non_standard_amino_acids_in_klifs(klifs_metadata):
+        """
+        For a given set of mol2 files, collect all non-standard amino acids.
 
-    stats = []
+        Parameters
+        ----------
+        klifs_metadata : pandas.DataFrame
+            KLIFS metadata describing every pocket entry in the KLIFS dataset.
 
-    for index, row in klifs_metadata.iterrows():
+        Returns
+        -------
+        dict
+            List of non-standard amino acids (value) for each structure contained in the input mol2 file (key).
+        """
 
-        print(f'{index+1}/{len(klifs_metadata)}')
+        # Get list of molecules linked to metadata
 
-        klifs_molecule_loader = KlifsMoleculeLoader(metadata_entry=row)
-        molecule = klifs_molecule_loader.molecule
+        molecules = []
+        for index, row in klifs_metadata.iterrows():
+            klifs_molecule_loader = KlifsMoleculeLoader(metadata_entry=row)
+            molecule = klifs_molecule_loader.molecule
+            molecules.append(molecule)
 
-        side_chain_orientation_feature = SideChainOrientationFeature(molecule)
-        points = side_chain_orientation_feature.from_molecule(molecule, verbose=True)
+        # 20 standard amino acids
+        standard_aminoacids = 'ALA ARG ASN ASP CYS GLN GLU GLY HIS ILE LEU LYS MET PHE PRO SER THR TRP TYR VAL'.split()
 
-        points['klifs_code'] = molecule.code
+        # Will contain per structure (key) list of non-standard amino acids (values)
+        non_standard_aminoacids = {}
 
-        stats.append(points)
+        for molecule in molecules:
 
-    return pd.concat(stats)
+            # Get deduplicated amino acid names
+            res_names = set(molecule.df.res_name)
+
+            # Retain only non-standard amino acids
+            non_standard_aminoacids_per_file = [res_name for res_name in res_names if res_name not in standard_aminoacids]
+
+            # If structure contains non-standard amino acids, add to dict
+            if non_standard_aminoacids_per_file:
+                non_standard_aminoacids[molecule.code] = non_standard_aminoacids_per_file
+
+        return non_standard_aminoacids
+
+
+
