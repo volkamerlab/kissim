5cc134ef44764eeb4d08d478651ce9028cb50256
Dominique Sydow <dominique.sydow@posteo.de>
2019-08-16 11:39:44 +0200

    Parallelise fingerprint generation

--- scripts/generate_fingerprints.py
+++ scripts/generate_fingerprints.py
@@ -3,6 +3,7 @@ This script generates all fingerprints for a given dataset.
 """
 
 import datetime
+import multiprocessing
 from pathlib import Path
 import pickle
 
@@ -11,26 +12,8 @@ import pandas as pd
 from kinsim_structure.auxiliary import KlifsMoleculeLoader, PdbChainLoader
 from kinsim_structure.encoding import Fingerprint
 
-# Load IO paths
-path_to_data = Path('/') / 'home' / 'dominique' / 'Documents' / 'data' / 'kinsim' / '20190724_full'
-path_to_kinsim = Path('/') / 'home' / 'dominique' / 'Documents' / 'projects' / 'kinsim_structure'
-path_to_results = path_to_kinsim / 'results'
 
-metadata_path = path_to_data / 'preprocessed' / 'klifs_metadata_preprocessed.csv'
-
-# Load metadata
-klifs_metadata = pd.read_csv(metadata_path)
-
-# Initialize lists to save results from looping
-fingerprints = []
-error_entries = []
-
-# Get start time
-start = datetime.datetime.now()
-
-for index, klifs_metadata_entry in klifs_metadata.iterrows():
-
-    print(f'{index + 1} / {klifs_metadata.shape[0]}')
+def get_fingerprint(klifs_metadata_entry):
 
     try:
         klifs_molecule_loader = KlifsMoleculeLoader(klifs_metadata_entry=klifs_metadata_entry)
@@ -42,26 +25,62 @@ for index, klifs_metadata_entry in klifs_metadata.iterrows():
         fp = Fingerprint()
         fp.from_molecule(molecule, chain)
 
-        fingerprint = fp.features
-        fingerprint['metadata_index'] = index
-        fingerprint['molecule_code'] = molecule.code
+        fp.features['metadata_index'] = klifs_metadata_entry.name
+        fp.features['molecule_code'] = molecule.code
 
-        fingerprints.append(fingerprint)
+        return fp
 
     except:
-        error_entries.append(klifs_metadata_entry)
 
-fingerprints = pd.concat(fingerprints)
+        with open(path_to_results / 'fingerprints_error_entries.txt', 'a+') as f_errors:
+            f_errors.write(
+                f'{klifs_metadata_entry.pdb_id}_'
+                f'{klifs_metadata_entry.chain}_'
+                f'{klifs_metadata_entry.alternate_model}\n'
+            )
 
-# Save fingerprints
-fingerprints.to_csv(path_to_results / 'fingerprints.csv')
 
-# Save problematic entries
-with open(path_to_results / 'fingerprints_error_entries.p', 'wb') as f:
-    pickle.dump(error_entries, f)
+if __name__ == "__main__":
 
-# Get start time
-end = datetime.datetime.now()
+    # Load IO paths
+    path_to_data = Path('/') / 'home' / 'dominique' / 'Documents' / 'data' / 'kinsim' / '20190724_full'
+    path_to_kinsim = Path('/') / 'home' / 'dominique' / 'Documents' / 'projects' / 'kinsim_structure'
+    path_to_results = path_to_kinsim / 'results'
 
-print(start)
-print(end)
+    metadata_path = path_to_data / 'preprocessed' / 'klifs_metadata_preprocessed.csv'
+
+    # Load metadata
+    klifs_metadata = pd.read_csv(metadata_path)
+
+    # Number of CPUs on machine
+    num_cores = multiprocessing.cpu_count() - 1
+
+    # Number of partitions to split DataFrame
+    num_partitions = num_cores
+
+    entry_list = [j for i, j in klifs_metadata.iterrows()]
+
+    # Create pool with `num_processes` processes
+    pool = multiprocessing.Pool(processes=num_cores)
+
+    start = datetime.datetime.now()
+
+    # Apply function to each chunk in list
+    fingerprints_list = pool.map(get_fingerprint, entry_list)
+
+    pool.close()
+    pool.join()
+
+    end = datetime.datetime.now()
+    print(start)
+    print(end)
+
+    # Save fingerprints
+    with open(path_to_results / 'fingerprints_parallelized.p', 'wb') as f:
+        pickle.dump(fingerprints_list, f)
+
+    fingerprints = [i.features for i in fingerprints_list if i]
+    fingerprints_df = pd.concat(fingerprints)
+
+    # Save fingerprints
+    fingerprints_df.to_csv(path_to_results / 'fingerprints_parallelized.csv')
