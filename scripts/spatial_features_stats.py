ae48e306fb5b97e55f1334a0ccbffc6a562c49df
Dominique Sydow <dominique.sydow@posteo.de>
2019-08-16 16:43:50 +0200

    Analysis of distances in dataset

---
+++ scripts/spatial_features_stats.py
@@ -0,0 +1,56 @@
+import multiprocessing
+from pathlib import Path
+import pickle
+
+import pandas as pd
+
+from kinsim_structure.auxiliary import KlifsMoleculeLoader
+from kinsim_structure.encoding import SpatialFeatures
+
+
+def get_spatial_features(klifs_metadata_entry):
+    space = SpatialFeatures()
+    ml = KlifsMoleculeLoader(klifs_metadata_entry=klifs_metadata_entry)
+    space.from_molecule(ml.molecule)
+
+    space.features['metadata_index'] = klifs_metadata_entry['index']
+    space.features['qualityscore'] = klifs_metadata_entry.qualityscore
+    space.features['resolution'] = klifs_metadata_entry.resolution
+
+    return space.features
+
+
+if __name__ == "__main__":
+
+    path_to_data = Path('/') / 'home' / 'dominique' / 'Documents' / 'data' / 'kinsim' / '20190724_full'
+    path_to_kinsim = Path('/') / 'home' / 'dominique' / 'Documents' / 'projects' / 'kinsim_structure'
+    path_to_results = path_to_kinsim / 'results'
+
+    metadata_path = path_to_data / 'preprocessed' / 'klifs_metadata_preprocessed.csv'
+
+    klifs_metadata = pd.read_csv(metadata_path)
+
+    # Number of CPUs on machine
+    num_cores = multiprocessing.cpu_count() - 1
+
+    # Number of partitions to split DataFrame
+    num_partitions = num_cores
+
+    entry_list = [j for i, j in klifs_metadata.iterrows()]
+    print(len(entry_list))
+
+    # Create pool with `num_processes` processes
+    pool = multiprocessing.Pool(processes=num_cores)
+
+    # Apply function to each chunk in list
+    space_list = pool.map(get_spatial_features, entry_list)
+    print(len(space_list))
+
+    pool.close()
+    pool.join()
+
+    all_distances = pd.concat(space_list)
+    print(len(all_distances))
+
+    with open(path_to_results / 'distances_all.p', 'wb') as f:
+        pickle.dump(all_distances, f)
