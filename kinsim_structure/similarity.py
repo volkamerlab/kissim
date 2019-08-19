3e8b2fd15bb0732ad96aae1d03ecb919e378ee72
Dominique Sydow <dominique.sydow@posteo.de>
2019-08-19 15:17:58 +0200

    Add get_pyschem_distances_similarity (w/wo weighting)

--- kinsim_structure/similarity.py
+++ kinsim_structure/similarity.py
@@ -11,16 +11,70 @@ import pandas as pd
 from scipy.spatial import distance
 
 
-def get_physchem_distances_similarity(fingerprint1, fingerprint2, physchem_weight=1, spatial_weight=1):
+def get_physchem_distances_similarity(pair, measure='modified_manhattan', weight=None):
+    """
+    Get similarity score for fingerprint type1 (consisting of physicochemical and distance properties) based on a
+    similarity measure (default modified Manhattan distance).
+    Option to weight physicochemical and distance properties differently (default None).
 
+    Parameters
+    ----------
+    pair : 2-element-list of kinsim_structure.encoding.Fingerprint
+        bla
+    measure : str
+        Similarity measure name.
+    weight : None or float [0,1]
+        If weight is None (default), fingerprint similarity is calculated over all positions.
+        If weight is given, similarity for physicochemical and distance fingerprint bits are calculated separately, and
+        summed up with respect to assigned weight for the phsicochemical part and (1-weight) for the distance part.
 
+    Returns
+    -------
+    List
+        List of molecule names in pair and their score.
+    """
 
-    physchem_score = None
-    spatial_score = None
+    if not weight:
 
-    score = physchem_weight * physchem_score + spatial_weight * spatial_score
+        score = calculate_similarity(
+            pair[0].features.iloc[:, 0:12],
+            pair[1].features.iloc[:, 0:12],
+            measure=measure
+        )
 
-    return score
+        return [
+            pair[0].molecule_code,
+            pair[1].molecule_code,
+            score
+        ]
+
+    else:
+
+        if 0 <= weight <= 1:
+
+            print(pair[0].molecule_code, pair[1].molecule_code)
+
+            physchem_score = calculate_similarity(
+                pair[0].features.iloc[:, 0:8],
+                pair[1].features.iloc[:, 0:8],
+                measure=measure
+            )
+            spatial_score = calculate_similarity(
+                pair[0].features.iloc[:, 9:12],
+                pair[1].features.iloc[:, 9:12],
+                measure=measure
+            )
+
+            score = weight * physchem_score + (1-weight) * spatial_score
+
+        else:
+            raise ValueError(f'Weight must be between 0 and 1. Given weight is: {weight}')
+
+        return [
+            pair[0].molecule_code,
+            pair[1].molecule_code,
+            score
+        ]
 
 
 def get_physchem_moments_similarity(fingerprint1, fingerprint2, physchem_weight=1, spatial_weight=1):
@@ -56,8 +110,6 @@ def calculate_similarity(fingerprint1, fingerprint2, measure='modified_manhattan
     if len(fingerprint1) != len(fingerprint2):
         raise ValueError(f'Input fingerprints must be of same length.')
 
-    print(type(fingerprint1))
-
     # Merge both fingerprints to array in order to remove positions with nan values
     fingerprints = np.array(
         [
@@ -71,13 +123,23 @@ def calculate_similarity(fingerprint1, fingerprint2, measure='modified_manhattan
         ~np.isnan(fingerprints).any(axis=1)
     ]
 
+    print(fingerprints_reduced)
+
     if measure == measures[0]:
 
         # Calculate inverse of the translated and scaled Manhattan distance
         fp1 = fingerprints_reduced[:, 0]
         fp2 = fingerprints_reduced[:, 1]
 
-        return 1 / (1 + 1 / len(fp1) * distance.cityblock(fp1, fp2))
+        try:
+
+            score = 1 / (1 + 1 / len(fp1) * distance.cityblock(fp1, fp2))
+
+        except ZeroDivisionError:
+
+            score = None
+
+        return score
 
     else:
         raise ValueError(f'Please choose a similarity measure: {", ".join(measures)}')
