cd1830e651cd1af8c712a0a998cff08244c51dde
Unknown <dominique.sydow@posteo.de>
2019-10-29 12:45:45 +0100

    Correct _normalize_physicochemical_bits

--- kinsim_structure/encoding.py
+++ kinsim_structure/encoding.py
@@ -345,18 +345,17 @@ class Fingerprint:
             # Normalize size
             normalized['size'] = normalized['size'].apply(lambda x: (x - 1) / 2.0)
 
-            # Normalize pharmacophoric features
+            # Normalize pharmacophoric features: HBD, HBA and charge
             normalized['hbd'] = normalized['hbd'].apply(lambda x: x / 3.0)
             normalized['hba'] = normalized['hba'].apply(lambda x: x / 2.0)
             normalized['charge'] = normalized['charge'].apply(lambda x: (x + 1) / 2.0)
-            normalized['aromatic'] = normalized['hba'].apply(lambda x: x / 1.0)
-            normalized['aliphatic'] = normalized['hba'].apply(lambda x: x / 1.0)
+
+            # No normalization needed for aromatic and aliphatic features which are already 0 or 1
 
             # Normalize side chain orientation
             normalized['sco'] = normalized['sco'].apply(lambda x: x / 180.0)
 
-            # Normalize exposure
-            normalized['exposure'] = normalized['exposure'].apply(lambda x: x / 1)
+            # No normalization needed for exposure feature which is already between 0 and 1
 
             return normalized
 
