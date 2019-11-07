81a0ff638c1a1654b9fe17947c5d93bdbe01df48
Dominique Sydow <dominique.sydow@posteo.de>
2019-11-07 16:05:15 +0100

    Reorder globals

--- kinsim_structure/encoding.py
+++ kinsim_structure/encoding.py
@@ -30,29 +30,6 @@ FEATURE_NAMES = {
     'moments': 'moment1 moment2 moment3'.split()
 }
 
-ANCHOR_RESIDUES = {
-    'hinge_region': [16, 47, 80],
-    'dfg_region': [19, 24, 81],
-    'front_pocket': [6, 48, 75]
-}  # Are the same as in Eva's implementation
-
-DISTANCE_CUTOFFS = {  # 99% percentile of all distances
-    'distance_to_centroid': (3.05, 21.38),
-    'distance_to_hinge_region': (4.10, 23.07),
-    'distance_to_dfg_region': (4.62, 26.69),
-    'distance_to_front_pocket': (5.46, 23.55)
-}
-
-MOMENT_CUTOFFS = {  # 99% percentile of all moments
-    'moment1': (11.68, 14.14),
-    'moment2': (3.29, 5.29),
-    'moment3': (-1.47, 4.66)
-}
-
-# KLIFS IDs for hinge/DFG region (taken from KLIFS website)
-HINGE_KLIFS_IDS = [46, 47, 48]
-DFG_KLIFS_IDS = [81, 82, 83]
-
 SITEALIGN_FEATURES = pd.read_csv(Path(__file__).parent / 'data' / 'sitealign_features.csv', index_col=0)
 
 MODIFIED_RESIDUE_CONVERSION = {
@@ -118,6 +95,29 @@ N_HEAVY_ATOMS_CUTOFF = {  # Number of heavy atoms needed for side chain centroid
     'TYR': 6,
     'TRP': 8
 }
+
+ANCHOR_RESIDUES = {
+    'hinge_region': [16, 47, 80],
+    'dfg_region': [19, 24, 81],
+    'front_pocket': [6, 48, 75]
+}  # Are the same as in Eva's implementation
+
+DISTANCE_CUTOFFS = {  # 99% percentile of all distances
+    'distance_to_centroid': (3.05, 21.38),
+    'distance_to_hinge_region': (4.10, 23.07),
+    'distance_to_dfg_region': (4.62, 26.69),
+    'distance_to_front_pocket': (5.46, 23.55)
+}
+
+MOMENT_CUTOFFS = {  # 99% percentile of all moments
+    'moment1': (11.68, 14.14),
+    'moment2': (3.29, 5.29),
+    'moment3': (-1.47, 4.66)
+}
+
+# KLIFS IDs for hinge/DFG region (taken from KLIFS website)
+HINGE_KLIFS_IDS = [46, 47, 48]
+DFG_KLIFS_IDS = [81, 82, 83]
 
 
 class FingerprintGenerator:
