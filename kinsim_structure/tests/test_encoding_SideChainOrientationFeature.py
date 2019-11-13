9c19bedad6751a4654e517cf34e2ec2e33941852
Unknown <dominique.sydow@posteo.de>
2019-11-13 15:24:29 +0100

    Update unit tests for SideChainOrientationFeature

--- kinsim_structure/tests/test_encoding_SideChainOrientationFeature.py
+++ kinsim_structure/tests/test_encoding_SideChainOrientationFeature.py
@@ -89,14 +89,129 @@ def test_get_ca(pdb_filename, chain_id, residue_id, ca):
         assert ca_calculated == ca
 
 
-@pytest.mark.parametrize('pdb_filename, chain_id, residue_id, side_chain_centroid', [
-    ('5i35.cif', 'A', 272, None),  # Residue is GLY
-    ('5i35.cif', 'A', 337, [63.66, 26.90, 23.41]),  # Residue is ALA with CB
+@pytest.mark.parametrize('pdb_filename, chain_id, residue_id', [
+    ('5i35.cif', 'A', 337),  # ALA
+    ('5i35.cif', 'A', 357),  # Non-standard residue
+])
+def test_get_pcb_from_gly_valueerror(pdb_filename, chain_id, residue_id):
+    """
+    Test exceptions in pseudo-CB calculation for GLY.
+
+    Parameters
+    ----------
+    pdb_filename : str
+        Path to cif file.
+    chain_id : str
+        Chain ID.
+    residue_id : int
+        Residue ID.
+    """
+
+    pdb_path = Path(__name__).parent / 'kinsim_structure' / 'tests' / 'data' / pdb_filename
+
+    pdb_chain_loader = PdbChainLoader(pdb_path=pdb_path, chain_id=chain_id)
+
+    chain = pdb_chain_loader.chain
+    try:
+        residue = chain[residue_id]
+    except KeyError:
+        # For non-standard residue MSE indexing did not work, thus use this workaround
+        residue = [i for i in chain.get_list() if i.get_id()[1] == residue_id][0]
+
+    with pytest.raises(ValueError):
+        feature = SideChainOrientationFeature()
+        feature._get_pcb_from_gly(residue)
+
+
+@pytest.mark.parametrize('pdb_filename, chain_id, residue_id, ca_pcb', [
+    ('5i35.cif', 'A', 272, np.array([52.24, 30.20, 32.25])),  # GLY
+])
+def test_get_pcb_from_gly(pdb_filename, chain_id, residue_id, ca_pcb):
+    """
+    Test pseudo-CB calculation for GLY.
+
+    Parameters
+    ----------
+    pdb_filename : str
+        Path to cif file.
+    chain_id : str
+        Chain ID.
+    residue_id : int
+        Residue ID.
+    ca_pcb : numpy.array
+        Pseudo-CB atom coordinates.
+    """
+
+    pdb_path = Path(__name__).parent / 'kinsim_structure' / 'tests' / 'data' / pdb_filename
+
+    pdb_chain_loader = PdbChainLoader(pdb_path=pdb_path, chain_id=chain_id)
+
+    chain = pdb_chain_loader.chain
+    try:
+        residue = chain[residue_id]
+    except KeyError:
+        # For non-standard residue MSE indexing did not work, thus use this workaround
+        residue = [i for i in chain.get_list() if i.get_id()[1] == residue_id][0]
+
+    feature = SideChainOrientationFeature()
+    ca_pcb_calculated = feature._get_pcb_from_gly(residue)
+
+    assert np.isclose(ca_pcb_calculated.get_array().mean(), ca_pcb.mean(), rtol=1e-04)
+
+
+@pytest.mark.parametrize('pdb_filename, chain_id, residue_id, ca_pcb', [
+    ('5i35.cif', 'A', 272, np.array([52.24, 30.20, 32.25])),  # GLY
+    ('5i35.cif', 'A', 337, np.array([64.06, 27.13, 23.97])),  # Residue is ALA with CB
     ('4jik.cif', 'A', 19, None),  # Residue is ALA without CB
-    ('5i35.cif', 'A', 336, [65.77, 23.74, 21.13]),  # Standard residue side chain with enough atoms
-    ('5l4q.cif', 'A', 130, [-15.11, -1.78, -18.79]),  # Standard residue side chain with too few atoms but CB atom
-    ('4jik.cif', 'A', 51, None),  # Standard residue side chain with too few atoms and no CB atom
-    ('5i35.cif', 'A', 357, [59.72, 14.73, 22.72]),  # Non-standard residue side chain with enough atoms (>0)
+])
+def test_get_pcb_from_residue(pdb_filename, chain_id, residue_id, ca_pcb):
+    """
+    Test pseudo-CB calculation for a residue.
+
+    Parameters
+    ----------
+    pdb_filename : str
+        Path to cif file.
+    chain_id : str
+        Chain ID.
+    residue_id : int
+        Residue ID.
+    ca_pcb : numpy.array
+        Pseudo-CB atom coordinates.
+    """
+
+    pdb_path = Path(__name__).parent / 'kinsim_structure' / 'tests' / 'data' / pdb_filename
+
+    pdb_chain_loader = PdbChainLoader(pdb_path=pdb_path, chain_id=chain_id)
+
+    chain = pdb_chain_loader.chain
+    try:
+        residue = chain[residue_id]
+    except KeyError:
+        # For non-standard residue MSE indexing did not work, thus use this workaround
+        residue = [i for i in chain.get_list() if i.get_id()[1] == residue_id][0]
+
+    feature = SideChainOrientationFeature()
+    feature.chain = chain  # Set chain value artificially
+    ca_pcb_calculated = feature._get_pcb_from_residue(residue, chain)
+
+    if ca_pcb is None:
+        assert ca_pcb_calculated is None
+    else:
+        assert np.isclose(ca_pcb_calculated.get_array().mean(), ca_pcb.mean(), rtol=1e-04)
+
+
+@pytest.mark.parametrize('pdb_filename, chain_id, residue_id, side_chain_centroid', [
+    ('5i35.cif', 'A', 272, [52.24, 30.20, 32.25]),  # GLY with pCB
+    ('3ot8.cif', 'A', 18, None),  # GLY without pCB (missing C and CA)
+    ('5i35.cif', 'A', 337, [63.66, 26.90, 23.41]),  # ALA with CB
+    ('3nlb.cif', 'A', 19, [9.77, -10.47, 10.16]),  # ALA with pCB
+    ('4jik.cif', 'A', 19, None),  # ALA without pCB (missing CA)
+    ('5i35.cif', 'A', 336, [65.77, 23.74, 21.13]),  # Standard residue (side chain) with enough atoms
+    ('5l4q.cif', 'A', 130, [-15.11, -1.78, -18.79]),  # Standard residue with too few atoms but CB atom
+    ('4yhf.cif', 'B', 421, [-2.95, -2.36, -14.49]),  # Standard residue with too few atoms, no CB atom, but pCB atom
+    ('4jik.cif', 'A', 51, None),  # Standard residue with too few atoms and no CB and pCB atom
+    ('5i35.cif', 'A', 357, [59.72, 14.73, 22.72]),  # Non-standard residue with enough atoms (>0)
     #('xxxx.cif', 'X', 0, None),  # Non-standard residue side chain with no atoms
     ('5l4q.cif', 'A', 57, [-27.53, 0.05, -41.01]),  # Side chain containing H atoms
 ])
@@ -128,7 +243,8 @@ def test_get_side_chain_centroid(pdb_filename, chain_id, residue_id, side_chain_
         residue = [i for i in chain.get_list() if i.get_id()[1] == residue_id][0]
 
     feature = SideChainOrientationFeature()
-    side_chain_centroid_calculated = feature._get_side_chain_centroid(residue)
+    side_chain_centroid_calculated = feature._get_side_chain_centroid(residue, chain)
+    print(side_chain_centroid_calculated)
 
     if side_chain_centroid_calculated and side_chain_centroid:
         # Check only x coordinate
@@ -180,7 +296,7 @@ def test_get_pocket_centroid(mol2_filename, pdb_filename, chain_id, pocket_centr
         'ABL1/2g2i_chainA/pocket.mol2',
         '2g2i.cif',
         'A',
-        {'ca': -16.21, 'side_chain_centroid': -16.18, 'pocket_centroid': -16.21}
+        {'ca': -16.2129, 'side_chain_centroid': -16.2073, 'pocket_centroid': -16.2129}
     )
 ])
 def test_get_pocket_vectors(mol2_filename, pdb_filename, chain_id, x_mean):
@@ -208,7 +324,7 @@ def test_get_pocket_vectors(mol2_filename, pdb_filename, chain_id, x_mean):
 
     feature = SideChainOrientationFeature()
     pocket_residues = feature._get_pocket_residues(klifs_molecule_loader.molecule, pdb_chain_loader.chain)
-    pocket_vectors = feature._get_pocket_vectors(pocket_residues)
+    pocket_vectors = feature._get_pocket_vectors(pocket_residues, pdb_chain_loader.chain)
 
     # Test if DataFrame contains correct columns
     pocket_vectors_columns = ['klifs_id', 'res_id', 'res_name', 'ca', 'side_chain_centroid', 'pocket_centroid']
@@ -225,14 +341,14 @@ def test_get_pocket_vectors(mol2_filename, pdb_filename, chain_id, x_mean):
     assert np.isclose(x_mean_calculated['ca'], x_mean['ca'], rtol=1e-03)
 
     # Test mean x coordinate of side chain centroid
-    assert np.isclose(x_mean_calculated['side_chain_centroid'], x_mean['side_chain_centroid'], rtol=1e-03)
+    assert np.isclose(x_mean_calculated['side_chain_centroid'], x_mean['side_chain_centroid'], rtol=1e-06)
 
     # Test mean x coordinate of pocket centroid
     assert np.isclose(x_mean_calculated['pocket_centroid'], x_mean['pocket_centroid'], rtol=1e-03)
 
 
 @pytest.mark.parametrize('mol2_filename, pdb_filename, chain_id, angles_mean', [
-    ('ABL1/2g2i_chainA/pocket.mol2', '2g2i.cif', 'A', 93.75)
+    ('ABL1/2g2i_chainA/pocket.mol2', '2g2i.cif', 'A', 95.07)
 ])
 def test_get_vertex_angles(mol2_filename, pdb_filename, chain_id, angles_mean):
     """
@@ -259,7 +375,7 @@ def test_get_vertex_angles(mol2_filename, pdb_filename, chain_id, angles_mean):
 
     feature = SideChainOrientationFeature()
     pocket_residues = feature._get_pocket_residues(klifs_molecule_loader.molecule, pdb_chain_loader.chain)
-    pocket_vectors = feature._get_pocket_vectors(pocket_residues)
+    pocket_vectors = feature._get_pocket_vectors(pocket_residues, pdb_chain_loader.chain)
     angles_calculated = feature._get_vertex_angles(pocket_vectors)
 
     assert list(angles_calculated.columns) == ['vertex_angle']
