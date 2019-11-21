8296162443c02ee70247cd8b1c40c0dd2f9791b5
Dominique Sydow <dominique.sydow@posteo.de>
2019-11-21 15:55:58 +0100

    mol2_to_pdb_converter.py: Rename script and run on unfiltered metadata

---
+++ examples/scripts/mol2_to_pdb_converter.py
@@ -0,0 +1,41 @@
+import logging
+
+from pathlib import Path
+import sys
+
+sys.path.append('../..')
+from kinsim_structure.preprocessing import KlifsMetadataLoader, Mol2ToPdbConverter
+
+PATH_SCRIPT = Path(__name__).parent
+PATH_KLIFS_DOWNLOAD = Path('/home/dominique/Documents/data/kinsim/20191115_full/KLIFS_download')
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    filename=PATH_SCRIPT / 'mol2_to_pdb_converter.log',
+    filemode='w',
+    level=logging.INFO
+)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+logging.getLogger('').addHandler(console)
+
+
+def main():
+
+    # Load metadata
+    klifs_metadata_loader = KlifsMetadataLoader()
+    klifs_metadata_loader.from_files(
+        PATH_KLIFS_DOWNLOAD / 'overview.csv',
+        PATH_KLIFS_DOWNLOAD / 'KLIFS_export.csv'
+    )
+    logger.info(f'Number of metadata entries: {len(klifs_metadata_loader.data_essential)}')
+
+    # Convert protein.mol2 to protein.pdb files
+    converter = Mol2ToPdbConverter()
+    converter.from_klifs_metadata(klifs_metadata_loader.data_essential, PATH_KLIFS_DOWNLOAD)
+
+
+if __name__ == "__main__":
+    main()
