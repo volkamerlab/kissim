faa65c6c1b2b04cf4732817b3745a6051b325d44
Dominique Sydow <dominique.sydow@posteo.de>
2019-11-22 15:32:48 +0100

    Add script: Mol2FormatScreener

---
+++ examples/scripts/mol2_format_screener.py
@@ -0,0 +1,63 @@
+"""
+mol2_format_screener.py
+
+Subpocket-based structural fingerprint for kinase pocket comparison.
+
+Screen all KLIFS protein.mol2 files for irregular formats, i.e. underscored residues, non-standard residues, and
+residues with duplicated atom names.
+"""
+
+import logging
+
+from pathlib import Path
+import pickle
+import sys
+
+sys.path.append('../..')
+from kinsim_structure.preprocessing import KlifsMetadataLoader, Mol2FormatScreener
+
+PATH_SCRIPT = Path(__name__).parent
+PATH_KLIFS_DOWNLOAD = Path('/home/dominique/Documents/data/kinsim/20191115_full/KLIFS_download')
+FILENAME = 'mol2_format_screener'
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    filename=PATH_SCRIPT / f'{FILENAME}.log',
+    filemode='w',
+    level=logging.INFO
+)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+logging.getLogger('').addHandler(console)
+
+
+def main():
+
+    # Load metadata
+    klifs_metadata_loader = KlifsMetadataLoader()
+    klifs_metadata_loader.from_files(
+        PATH_KLIFS_DOWNLOAD / 'overview.csv',
+        PATH_KLIFS_DOWNLOAD / 'KLIFS_export.csv'
+    )
+    logger.info(f'Number of metadata entries: {len(klifs_metadata_loader.data_essential)}')
+
+    # Screen protein.mol2 file for irregular formats
+    klifs_metadata = klifs_metadata_loader.data_essential
+    mol2_format_screener = Mol2FormatScreener(klifs_metadata, PATH_KLIFS_DOWNLOAD)
+
+    logger.info(f'Number of structures with underscored residues: '
+                f'{len(mol2_format_screener.residues_underscored.groupby("filepath"))}')
+    logger.info(f'Number of structures with non-standard residues: '
+                f'{len(mol2_format_screener.residues_non_standard.groupby("filepath"))}')
+    logger.info(f'Number of structures with duplicated residue atom names: '
+                f'{len(mol2_format_screener.residues_duplicated_atom_names.groupby("filepath"))}')
+
+    # Save Mol2FormatScreener object to disc
+    with open(PATH_KLIFS_DOWNLOAD / f'{FILENAME}.p', 'wb') as f:
+        pickle.dump(mol2_format_screener, f)
+
+
+if __name__ == "__main__":
+    main()
