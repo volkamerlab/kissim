fa94936bf9e494cb2fc88b175540903aa956c425
Dominique Sydow <dominique.sydow@posteo.de>
2019-11-25 10:28:20 +0100

    Add script: Mol2KlifsToPymolFormatter

---
+++ examples/scripts/mol2_klifs_to_pymol_formatter.py
@@ -0,0 +1,56 @@
+"""
+mol2_klifs_to_pymol_formatter.py
+
+Subpocket-based structural fingerprint for kinase pocket comparison.
+
+Format KLIFS mol2 file to PyMol readable mol2 file.
+"""
+
+import logging
+
+from pathlib import Path
+import pickle
+import sys
+
+sys.path.append('../..')
+from kinsim_structure.preprocessing import KlifsMetadataLoader, Mol2KlifsToPymolFormatter
+
+PATH_SCRIPT = Path(__name__).parent
+PATH_KLIFS_DOWNLOAD = Path('/home/dominique/Documents/data/kinsim/20191115_full/KLIFS_download')
+FILENAME = 'mol2_klifs_to_pymol_formatter'
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    filename=PATH_SCRIPT / f'{FILENAME}.log',
+    filemode='w',
+    level=logging.INFO
+)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+logging.getLogger('').addHandler(console)
+
+
+def main():
+
+    # Load metadata
+    klifs_metadata_loader = KlifsMetadataLoader()
+    klifs_metadata_loader.from_files(
+        PATH_KLIFS_DOWNLOAD / 'overview.csv',
+        PATH_KLIFS_DOWNLOAD / 'KLIFS_export.csv'
+    )
+
+    # Screen protein.mol2 file for irregular formats
+    klifs_metadata = klifs_metadata_loader.data_essential
+    logger.info(f'Number of metadata entries: {len(klifs_metadata)}')
+    mol2_klifs_to_pymol_formatter = Mol2KlifsToPymolFormatter()
+    mol2_klifs_to_pymol_formatter.from_metadata(klifs_metadata, PATH_KLIFS_DOWNLOAD)
+
+    # Save Mol2FormatScreener object to disc
+    with open(PATH_KLIFS_DOWNLOAD.parent / f'{FILENAME}.p', 'wb') as f:
+        pickle.dump(mol2_klifs_to_pymol_formatter, f)
+
+
+if __name__ == "__main__":
+    main()
