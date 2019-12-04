50fd19508983881ea60b635aba7de1975e938151
Dominique Sydow <dominique.sydow@posteo.de>
2019-12-04 10:36:11 +0100

    Update unit test for Mol2KlifsToPymolConverter

--- kinsim_structure/tests/test_preprocessing_Mol2KlifsToPymolConverter.py
+++ kinsim_structure/tests/test_preprocessing_Mol2KlifsToPymolConverter.py
@@ -13,9 +13,9 @@ PATH_TEST_DATA = Path(__name__).parent / 'kinsim_structure' / 'tests' / 'data'
 
 @pytest.mark.parametrize('path_mol2, path_mol2_pymol, n_converted_lines', [
     (
-        PATH_TEST_DATA / 'KLIFS_download' / 'HUMAN/TBK1/4iwq_chainA/protein.mol2',
-        PATH_TEST_DATA / 'KLIFS_download' / 'HUMAN/TBK1/4iwq_chainA/protein_pymol.mol2',
-        13
+        PATH_TEST_DATA / 'Mol2KlifsToPymolConverter' / 'protein.mol2',
+        PATH_TEST_DATA / 'Mol2KlifsToPymolConverter' / 'protein_pymol.mol2',
+        15
     )
 ])
 def test_convert_mol2(path_mol2, path_mol2_pymol, n_converted_lines):
@@ -41,10 +41,10 @@ def test_convert_mol2(path_mol2, path_mol2_pymol, n_converted_lines):
 
     # Convert KLIFS mol2 file
     converter = Mol2KlifsToPymolConverter()
-    lines_mol2_pymol_calculated = converter._convert_mol2(lines_mol2, 'molecule1')
+    lines_new_calculated, lines_converted_calculated = converter._convert_mol2(lines_mol2, 'molecule1')
 
     # Test if conversion is correct
-    assert lines_mol2_pymol_calculated == lines_mol2_pymol
+    assert lines_new_calculated == lines_mol2_pymol
 
     # Test if converted lines are correctly assigned to class attribute
-    assert len(converter.converted_lines) == n_converted_lines
+    assert len(lines_converted_calculated) == n_converted_lines
