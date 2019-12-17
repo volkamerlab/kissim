2cc3ce27d00484e34f43f962fa74231215d9d139
Dominique Sydow <dominique.sydow@posteo.de>
2019-12-17 09:25:30 +0100

    Remove examples folder (finally!!!)

--- examples/results/postprocessing/pymol_large_distances.py
+++
@@ -1,6 +0,0 @@
-from pymol import *
-import os
-from pymol.cgo import *
-
-cmd.load("/home/dominique/Documents/data/kinsim/20190724_full/raw/KLIFS_download/HUMAN/ITK/4qd6_altA_chainA/protein.mol2", "4qd6_altA_chainA")
-cmd.load("/home/dominique/Documents/data/kinsim/20190724_full/raw/KLIFS_download/HUMAN/PBK/5j0a_chainA/protein.mol2", "5j0a_chainA")
