b3b124a451f594e4c52bcb9b7241e42fd46eb848
Unknown <dominique.sydow@posteo.de>
2020-10-15 10:59:18 +0200

    Add docstrings to io module

--- kissim/io/biopython.py
+++ kissim/io/biopython.py
@@ -14,49 +14,117 @@ from .core import Pocket
 
 class PocketBiopython(Pocket):
     """
-    Class defining the base for structural objects for this package. TODO
+    Class defining the Biopython-based pocket object.
+
+    Attributes
+    ----------
+    _residue_ids : list of int
+        Pocket residue IDs.
+    _data_complex : Bio.PDB.Structure.Structure
+        Structural data for the full complex (not the pocket only).
+    _hse_ca_complex : Bio.PDB.HSExposure.HSExposureCA
+        CA exposures for the full complex (not the pocket only).
+    _hse_cb_complex : Bio.PDB.HSExposure.HSExposureCB
+        CB exposures for the full complex (not the pocket only).
     """
 
     def __init__(self):
 
+        self._residue_ids = None
         self._data_complex = None
-        self._pocket_residue_ids = None
-        self._hse_ca = None
-        self._hse_cb = None
+        self._hse_ca_complex = None
+        self._hse_cb_complex = None
 
     @classmethod
     def from_local(cls, local, structure_id):
-        """TODO"""
+        """
+        Get Biopython-based pocket object from a KLIFS structure ID (locally).
+
+        Parameters
+        ----------
+        local : opencadd.databases.klifs.session.Session
+            Local KLIFS session.
+        structure_id : int
+            KLIFS structure ID.
+
+        Returns
+        -------
+        kissim.io.dataframe.PocketDataframe
+            Biopython-based pocket object.
+        """
 
         return cls._from_backend(local, structure_id)
 
     @classmethod
     def from_remote(cls, remote, structure_id):
-        """TODO"""
+        """
+        Get Biopython-based pocket object from a KLIFS structure ID (remotely).
+
+        Parameters
+        ----------
+        local : opencadd.databases.klifs.session.Session
+            Remote KLIFS session.
+        structure_id : int
+            KLIFS structure ID.
+
+        Returns
+        -------
+        kissim.io.dataframe.PocketDataframe
+            Biopython-based pocket object.
+        """
 
         return cls._from_backend(remote, structure_id)
 
     @classmethod
     def _from_backend(cls, backend, structure_id):
-        """TODO"""
+        """
+        Get Biopython-based pocket object from a KLIFS structure ID.
+
+        Parameters
+        ----------
+        backend : opencadd.databases.klifs.session.Session
+            Local or remote KLIFS session.
+        structure_id : int
+            KLIFS structure ID.
+
+        Returns
+        -------
+        kissim.io.dataframe.PocketDataframe
+            Biopython-based pocket object.
+        """
 
         pocket = cls()
         pocket._data_complex = pocket._get_biopython(backend, structure_id)
-        pocket._pocket_residue_ids = pocket._get_pocket_residue_ids(backend, structure_id)
+        pocket._residue_ids = pocket._get_pocket_residue_ids(backend, structure_id)
         # Cast residue IDs str > int (necessary for Biopython where they are int)
-        pocket._pocket_residue_ids = [int(i) for i in pocket._pocket_residue_ids]
-        pocket._hse_ca = HSExposure.HSExposureCA(pocket._data_complex)
-        pocket._hse_cb = HSExposure.HSExposureCB(pocket._data_complex)
+        pocket._residue_ids = [int(i) for i in pocket._residue_ids]
+        pocket._hse_ca_complex = HSExposure.HSExposureCA(pocket._data_complex)
+        pocket._hse_cb_complex = HSExposure.HSExposureCB(pocket._data_complex)
         return pocket
 
     @property
     def residue_ids(self):
-        """TODO"""
-        return self._pocket_residue_ids
+        """
+        Pocket residue IDs.
+
+        Returns
+        -------
+        list of int
+            Pocket residue IDs.
+        """
+
+        return self._residue_ids
 
     @property
     def centroid(self):
-        """TODO"""
+        """
+        Pocket centroid.
+
+        Returns
+        -------
+        Bio.PDB.vectors.Vector
+            Coordinates for the pocket centroid.
+        """
 
         ca_atoms = self.ca_atoms
         ca_atom_vectors = ca_atoms["ca.atom"].to_list()
@@ -68,7 +136,17 @@ class PocketBiopython(Pocket):
 
     @property
     def ca_atoms(self):
-        """TODO"""
+        """
+        Pocket CA atoms.
+
+        Returns
+        -------
+        pandas.DataFrame
+            Pocket CA atoms (rows) with the following columns:
+            - "residue.id": Residue ID
+            - "ca.atom": CA atom (Bio.PDB.Atom.Atom)
+            - "ca.vector": CA atom vector (Bio.PDB.vectors.Vector)
+        """
 
         ca_atoms = []
         for residue_id in self.residue_ids:
@@ -89,7 +167,16 @@ class PocketBiopython(Pocket):
 
     @property
     def pcb_atoms(self):
-        """TODO"""
+        """
+        Pocket pseudo-CB atoms.
+
+        Returns
+        -------
+        pandas.DataFrame
+            Pocket pseudo-CB atoms (rows) with the following columns:
+            - "residue.id": Residue ID
+            - "ca.vector": Pseudo-CB atom vector (Bio.PDB.vectors.Vector)
+        """
 
         pcb_atoms = []
         for residue_id in self.residue_ids:
@@ -101,7 +188,17 @@ class PocketBiopython(Pocket):
 
     @property
     def side_chain_representatives(self):
-        """TODO"""
+        """
+        Pocket residues' side chain representatives.
+
+        Returns
+        -------
+        pandas.DataFrame
+            Pocket residues' side chain representatives (rows) with the following columns:
+            - "residue.id": Residue ID
+            - "sc.atom": Side chain representative (Bio.PDB.Atom.Atom or None)
+            - "sc.vector": Side chain representative vector (Bio.PDB.vectors.Vector or None)
+        """
 
         sc_atoms = []
         for residue_id in self.residue_ids:
@@ -120,8 +217,72 @@ class PocketBiopython(Pocket):
 
         return sc_atoms
 
+    @property
+    def hse_ca(self):
+        """
+        CA exposures for pocket residues.
+
+        Returns
+        -------
+        dict of tuple: tuple
+            CA exposures (values) for pocket residues (keys):
+
+        Notes
+        -----
+        Keys and values follow the Biopython notation:
+        - Key: (chain id, (hetero-flag, sequence identifier, insertion code))
+          - chain id: str (chain ID)
+          - hetero-flag: str ("H_" for hetero atoms, "W" for water)
+          - sequence identifier: int (residue ID)
+          - insertion code: str (mark insertions)
+        - Value: (hse_up, hse_down, angle)
+          - hse_up: int (number of CA atoms in upper sphere)
+          - hse_down: int (number of CA atoms in lower sphere)
+          - angle: float (angle between CA-CB and CA-pCB)
+        """
+
+        return {
+            residue: exposure
+            for residue, exposure in self._hse_ca_complex.property_dict.items()
+            if residue[1][1] in self.residue_ids
+        }
+
+    @property
+    def hse_cb(self):
+        """
+        CB exposures for pocket residues.
+
+        Returns
+        -------
+        dict of tuple: tuple
+            CA exposures (values) for pocket residues (keys):
+
+        Notes
+        -----
+        See notes in hse_ca property.
+        """
+
+        return {
+            residue: exposure
+            for residue, exposure in self._hse_cb_complex.property_dict.items()
+            if residue[1][1] in self.residue_ids
+        }
+
     def _ca_atom(self, residue_id):
-        """TODO"""
+        """
+        Get the CA atom for a residue.
+
+        Parameters
+        ----------
+        residue_id : int
+            Residue ID.
+
+        Returns
+        -------
+        Bio.PDB.Atom.Atom or None
+            Residue's CA atom.
+        """
+
         residue = self._residue_from_residue_id(residue_id)
         try:
             return residue["CA"]
@@ -129,11 +290,22 @@ class PocketBiopython(Pocket):
             return None
 
     def _pcb_atom(self, residue_id):
-        """TODO"""
+        """
+        Get the pseudo-CB coordinates for a residue.
+
+        Parameters
+        ----------
+        residue_id : int
+            Residue ID.
+
+        Returns
+        -------
+        Bio.PDB.vectors.Vector or None
+            Pseudo-CB atom coordinates.
+        """
 
         # Get biopython residue object
         residue = self._residue_from_residue_id(residue_id)
-
         # Get pCB atom
         if residue.get_resname() == "GLY":
             pcb = self._pcb_atom_from_gly(residue)
@@ -150,7 +322,19 @@ class PocketBiopython(Pocket):
             return None
 
     def _pcb_atom_from_non_gly(self, residue):
-        """TODO"""
+        """
+        Get the pseudo-CB atom for a non-GLY residue.
+
+        Parameters
+        ----------
+        residue : Bio.PDB.Residue.Residue
+            Residue (not GLY).
+
+        Returns
+        -------
+        Bio.PDB.vectors.Vector or None
+            Pseudo-CB atom coordinates.
+        """
 
         residue_id = residue.id[1]
 
@@ -166,21 +350,47 @@ class PocketBiopython(Pocket):
                 return None
 
             # Get pseudo-CB for non-GLY residue
-            pcb = self._hse_ca._get_cb(residue_before, residue, residue_after)
+            pcb = self._hse_ca_complex._get_cb(residue_before, residue, residue_after)
+            # Keep only pseudo-CB coordinates (drop angle between CA-CB and CA-pCB)
+            pcb = pcb[0]
             return pcb
 
     def _pcb_atom_from_gly(self, residue):
-        """TODO"""
+        """
+        Get the pseudo-CB atom for a GLY residue.
+
+        Parameters
+        ----------
+        residue : Bio.PDB.Residue.Residue
+            GLY residue.
+
+        Returns
+        -------
+        Bio.PDB.vectors.Vector or None
+            Pseudo-CB atom coordinates.
+        """
 
         if residue.get_resname() != "GLY":
             raise ValueError(f"Residue must be GLY, but is {residue.get_resname()}.")
         else:
             # Get pseudo-CB for GLY (vector centered at origin)
-            pcb = self._hse_cb._get_gly_cb_vector(residue)
+            pcb = self._hse_cb_complex._get_gly_cb_vector(residue)
             return pcb
 
     def _side_chain_representative(self, residue_id):
-        """TODO"""
+        """
+        Get the side chain representative for a residue.
+
+        Parameters
+        ----------
+        residue_id : int
+            Residue ID.
+
+        Returns
+        -------
+        Bio.PDB.Atom.Atom or None
+            Side chain representative.
+        """
 
         # Get biopython residue object
         residue = self._residue_from_residue_id(residue_id)
@@ -194,7 +404,19 @@ class PocketBiopython(Pocket):
             return None
 
     def _residue_from_residue_id(self, residue_id):
-        """TODO"""
+        """
+        Get the residue from a residue ID.
+
+        Parameters
+        ----------
+        residue_id : int
+            Residue ID.
+
+        Returns
+        -------
+        Bio.PDB.Residue.Residue
+            Residue.
+        """
 
         residues = list(self._data_complex.get_residues())
         residue = [residue for residue in residues if residue.get_id()[1] == residue_id]
--- kissim/io/core.py
+++ kissim/io/core.py
@@ -16,27 +16,97 @@ class Pocket:
 
     @classmethod
     def from_local(cls, local, structure_id):
-        """TODO"""
+        """
+        Get pocket object from a KLIFS structure ID (locally).
+
+        Parameters
+        ----------
+        local : opencadd.databases.klifs.session.Session
+            Local KLIFS session.
+        structure_id : int
+            KLIFS structure ID.
+
+        Returns
+        -------
+        kissim.io.dataframe.PocketDataframe or kissim.io.dataframe.PocketBiopython
+            Pocket object.
+        """
         raise NotImplementedError("Implement in your subclass!")
 
     @classmethod
     def from_remote(cls, remote, structure_id):
-        """TODO"""
+        """
+        Get pocket object from a KLIFS structure ID (remotely).
+
+        Parameters
+        ----------
+        remote : opencadd.databases.klifs.session.Session
+            Local KLIFS session.
+        structure_id : int
+            KLIFS structure ID.
+
+        Returns
+        -------
+        kissim.io.dataframe.PocketDataframe or kissim.io.dataframe.PocketBiopython
+            Pocket object.
+        """
         raise NotImplementedError("Implement in your subclass!")
 
     @classmethod
     def _from_backend(cls, backend, structure_id):
-        """TODO"""
+        """
+        Get pocket object from a KLIFS structure ID.
+
+        Parameters
+        ----------
+        backend : opencadd.databases.klifs.session.Session
+            Local or remote KLIFS session.
+        structure_id : int
+            KLIFS structure ID.
+
+        Returns
+        -------
+        kissim.io.dataframe.PocketDataframe or kissim.io.dataframe.PocketBiopython
+            Pocket object.
+        """
         raise NotImplementedError("Implement in your subclass!")
 
     def _get_dataframe(self, backend, structure_id):
-        """TODO"""
+        """
+        Get structural data for a complex from a KLIFS structure ID as DataFrame.
+
+        Parameters
+        ----------
+        backend : opencadd.databases.klifs.session.Session
+            Local or remote KLIFS session.
+        structure_id : int
+            KLIFS structure ID.
+
+        Returns
+        -------
+        pandas.DataFrame
+            Structural data for a complex.
+        """
 
         dataframe = backend.coordinates.to_dataframe(structure_id, "complex", "pdb")
         return dataframe
 
     def _get_biopython(self, backend, structure_id):
-        """TODO"""
+        """
+        Get structural data for a complex from a KLIFS structure ID as Biopython Structure object.
+
+        Parameters
+        ----------
+        backend : opencadd.databases.klifs.session.Session
+            Local or remote KLIFS session.
+        structure_id : int
+            KLIFS structure ID.
+
+        Returns
+        -------
+        pandas.DataFrame
+            Structural data for a complex.
+        """
 
         with enter_temp_directory():
             filepath = backend.coordinates.to_pdb(structure_id, "complex")
@@ -44,7 +114,21 @@ class Pocket:
             return biopython
 
     def _get_pocket_residue_ids(self, backend, structure_id):
-        """TODO"""
+        """
+        Get pocket residues.
+
+        Parameters
+        ----------
+        backend : opencadd.databases.klifs.session.Session
+            Local or remote KLIFS session.
+        structure_id : int
+            KLIFS structure ID.
+
+        Returns
+        -------
+        list of str
+            Pocket residues.
+        """
 
         pocket_residues = backend.pockets.by_structure_id(structure_id)
         pocket_residue_ids = pocket_residues["residue.id"].to_list()
