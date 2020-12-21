7127f8b8f76bf08c7a4b0d2957f9d664861d3251
Unknown <dominique.sydow@posteo.de>
2020-12-21 20:07:44 +0100

    PocketDataFrame: Unit tests are done in opencadd (KlifsPocket subclass)

---
+++ kissim/tests/test_io_pocketdataframe.py
@@ -0,0 +1,13 @@
+"""
+Unit and regression test for kissim.io.PocketDataFrame class methods.
+
+Note that class 
+- methods (`from_structure_klifs_id`)
+- attributes (`_residue_ids`, `residue_ixs`) and 
+- properties (`residues`)
+are tested in test_io.py.
+
+Note also that PocketDataFrame is a subclass of opencadd's KlifsPocket class, 
+which is tested thouroughly in opencadd:
+- properties (`data`, `center`, `ca_atoms`)
+"""
