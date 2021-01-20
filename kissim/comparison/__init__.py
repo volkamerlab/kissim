26298af62c23f3277cbd461ed7c1ca4cea305dcc
Unknown <dominique.sydow@posteo.de>
2021-01-20 09:54:58 +0100

    Update __init__

--- kissim/comparison/__init__.py
+++ kissim/comparison/__init__.py
@@ -1,10 +1,8 @@
 """
-kissim.similarity
+kissim.comparison
 """
 
-from .api import (
-    FeatureDistances,
-    FeatureDistancesGenerator,
-    FingerprintDistance,
-    FingerprintDistanceGenerator,
-)
+from .feature_distances import FeatureDistances
+from .feature_distances_generator import FeatureDistancesGenerator
+from .fingerprint_distance import FingerprintDistance
+from .fingerprint_distance_generator import FingerprintDistanceGenerator
