b5719bfb510e6cf5f085bab39c93ae77ecf38dce
Unknown <dominique.sydow@posteo.de>
2021-01-21 15:51:13 +0100

    Minor test name change

--- kissim/tests/api/test_encode.py
+++ kissim/tests/api/test_encode.py
@@ -12,7 +12,7 @@ from kissim.api import encode
     "structure_klifs_ids, json_filepath, n_cores, local_klifs_session",
     [([110], None, 1, None)],
 )
-def test_encode_bla(structure_klifs_ids, json_filepath, n_cores, local_klifs_session):
+def test_encode(structure_klifs_ids, json_filepath, n_cores, local_klifs_session):
 
     with enter_temp_directory():
         fingerprint_generator = encode(
