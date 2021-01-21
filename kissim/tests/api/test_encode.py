d1a57659494a71d16069eba30ea1220651e00bd8
Unknown <dominique.sydow@posteo.de>
2021-01-21 15:45:50 +0100

    Add API encode unit tests

--- kissim/tests/api/test_encode.py
+++ kissim/tests/api/test_encode.py
@@ -2,24 +2,17 @@
 Unit and regression test for the kissim.api.encode module.
 """
 
-from pathlib import Path
-
 import pytest
 
 from kissim.utils import enter_temp_directory
 from kissim.api import encode
 
-PATH_TEST_DATA = Path(__name__).parent / "kissim/tests/data/KLIFS_download"
-
 
 @pytest.mark.parametrize(
     "structure_klifs_ids, json_filepath, n_cores, local_klifs_session",
-    [
-        ([12347], None, 1, None),
-        ([12347], "fingerprints.json", 1, PATH_TEST_DATA),
-    ],
+    [([110], None, 1, None)],
 )
-def test_encode(structure_klifs_ids, json_filepath, n_cores, local_klifs_session):
+def test_encode_bla(structure_klifs_ids, json_filepath, n_cores, local_klifs_session):
 
     with enter_temp_directory():
         fingerprint_generator = encode(
