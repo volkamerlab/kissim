7ee399d77484282055bc98c950f168356102accb
Unknown <dominique.sydow@posteo.de>
2021-01-25 14:17:08 +0100

    Update kissim __init__.py w.r.t. cookiecutter 1.5

--- kissim/__init__.py
+++ kissim/__init__.py
@@ -3,10 +3,12 @@ kissim
 Subpocket-based structural fingerprint for kinase pocket comparison
 """
 
+# Add imports here
+from .kissim import *
+
 # Handle versioneer
 from ._version import get_versions
-
 versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
 del get_versions, versions
