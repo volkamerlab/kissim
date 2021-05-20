6864b6c08776b96631310313d1a22796bcb6b825
Unknown <dominique.sydow@posteo.de>
2021-05-20 10:29:05 +0200

    Unit test: Add check for kinase annotation output

--- kissim/tests/api/test_api_compare.py
+++ kissim/tests/api/test_api_compare.py
@@ -34,10 +34,14 @@ def test_compare(fingerprint_generator, output_path, feature_weights, n_cores):
         assert fingerprint_distance_filepath.exists()
         fingerprint_distance_filepath.unlink()
 
-        feature_distances_filepath = output_path / "fingerprint_distances_to_kinase_matrix.csv"
-        assert feature_distances_filepath.exists()
-        feature_distances_filepath.unlink()
+        kinase_matrix_filepath = output_path / "fingerprint_distances_to_kinase_matrix.csv"
+        assert kinase_matrix_filepath.exists()
+        kinase_matrix_filepath.unlink()
 
-        feature_distances_filepath = output_path / "fingerprint_distances_to_kinase_clusters.tree"
-        assert feature_distances_filepath.exists()
-        feature_distances_filepath.unlink()
+        kinase_tree_filepath = output_path / "fingerprint_distances_to_kinase_clusters.tree"
+        assert kinase_tree_filepath.exists()
+        kinase_tree_filepath.unlink()
+
+        kinase_annotation_filepath = output_path / "kinase_annotation.csv"
+        assert kinase_annotation_filepath.exists()
+        kinase_annotation_filepath.unlink()
